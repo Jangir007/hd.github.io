714cc425465a4aebc62634ef345d56358b764802
Jangir007 <assiyaespana2020@gmail.com>
2025-02-21 21:22:20 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+    let hiddenRow = $(".expandable");
+    $(".expand-btn").click(function () {
+        hiddenRow.toggleClass("hidden");
+        if (hiddenRow.is(":visible")) {
+            $(".expand-btn").html("Decrease");
+        }
+        else {
+            $(".expand-btn").html("Expand");
+        }
+    });
+});
+
+
